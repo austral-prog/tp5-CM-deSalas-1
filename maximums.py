5ccb616ed19749a8442b16841744d10984fd6e66
CM-deSalas <cmdesalas@mail.austral.edu.ar>
2025-09-12 11:33:54 -0300

    Update maximums.py

--- maximums.py
+++ maximums.py
@@ -5,10 +5,10 @@ def max_of_two(x, y):
         return y
 
 def max_of_three(x, y, z):
-    if x < y > z:
+    if x < y and y > z:
         return y
-    elif y < x > z:
+    elif y < x and x > z:
         return x
-    elif x < z > y:
+    elif x < z and z > y:
         return z
 
